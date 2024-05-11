82c691e5520466d828a4007bf24c804b1c81e5f4
Sergio Sanz <ssanz.ceb@outlook.com>
2024-05-11 19:38:34 +0200

    Refactor

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
 class Settings:
     def __init__(self, ss_game):
-        """Store the game settings of Sideways Shooter"""
-        screen_rect = ss_game.screen.get_rect()
-        
+        """Store the game settings of Sideways Shooter"""        
         # Screen settings
-        self.screen_width, self.screen_height = screen_rect.size
+        self.screen_width, self.screen_height = ss_game.screen.get_size()
         self.bg_color = (230, 230, 230)
+
+        # Bullet settings
+        self.bullet_width, self.bullet_height = (3, 15)
+        self.bullet_speed = 2.5
--- sideways_shooter.py
+++ sideways_shooter.py
@@ -6,6 +6,7 @@ from settings import Settings
 from ship import Ship
 class SidewaysShooter:
     def __init__(self):
+        pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
         self.clock = pygame.time.Clock()
         self.settings = Settings(self)
@@ -22,17 +23,17 @@ class SidewaysShooter:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            self._check_keydown_events(event)
+            if event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
             if event.type == pygame.KEYUP:
                 self.ship.direction = 0
 
     def _check_keydown_events(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                self.ship.direction = -1
-            elif event.key == pygame.K_DOWN:
-                self.ship.direction = 1
-
+        if event.key == pygame.K_UP:
+            self.ship.direction = -1
+        elif event.key == pygame.K_DOWN:
+            self.ship.direction = 1
+    
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.screen.blit(self.ship.image, self.ship.rect)
