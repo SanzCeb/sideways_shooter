edcb97e603f427b71df902e47075edea3da50460
Sergio Sanz <ssanz.ceb@outlook.com>
2024-05-11 11:44:11 +0200

    Basic ship movement

--- ship.py
+++ ship.py
@@ -5,3 +5,13 @@ class Ship:
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.rect.midleft = ss_game.screen.get_rect().midleft
+
+        self.y = float(self.rect.y)
+    
+    def move_up(self):
+        self.y -= 1
+        self.rect.y = self.y
+    
+    def move_down(self):
+        self.y += 1
+        self.rect.y = self.y
---
+++ tests/test_ship.py
@@ -0,0 +1,25 @@
+import pytest 
+
+from ship import Ship
+from sideways_shooter import SidewaysShooter
+
+@pytest.fixture
+def ship():
+    ss_game = SidewaysShooter()
+    ship = Ship(ss_game)
+    return ship
+
+
+def test_ship_move_up(ship):
+    old_pos = (ship.rect.x, ship.rect.y)
+    ship.move_up()
+    new_pos = (ship.rect.x, ship.rect.y)
+    assert old_pos[0] == old_pos[0]
+    assert old_pos[1] > new_pos[1]
+
+def test_ship_move_down(ship):
+    old_pos = (ship.rect.x, ship.rect.y)
+    ship.move_down()
+    new_pos = (ship.rect.x, ship.rect.y)
+    assert old_pos[0] == old_pos[0]
+    assert old_pos[1] < new_pos[1]
