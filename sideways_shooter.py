b4fc96f57df7c9a74be18896c9b45e06a0f00389
Sergio Sanz <ssanz.ceb@outlook.com>
2024-05-11 11:01:41 +0200

    Make the screen gray

--- sideways_shooter.py
+++ sideways_shooter.py
@@ -5,13 +5,14 @@ import pygame
 class SidewaysShooter:
     def __init__(self):
         self.screen = pygame.display.set_mode((1200, 800))
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
+            self.screen.fill(self.bg_color)
             pygame.display.flip()
 
 if __name__ == '__main__':
