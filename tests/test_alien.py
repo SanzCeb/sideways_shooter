28e909fbabd40ee2a63f56a0ad1b7c2da53259a1
Sergio Sanz <ssanz.ceb@outlook.com>
2024-05-15 08:13:22 +0200

    test_alien refactor

--- tests/test_alien.py
+++ tests/test_alien.py
@@ -1,8 +1,11 @@
+import pytest
 from mocks import mock_sideways_shooter
 
-def test_alien_goes_up_and_down(mock_sideways_shooter):
-    alien = mock_sideways_shooter.aliens.sprites()[0]
+@pytest.fixture
+def alien(mock_sideways_shooter):
+    return mock_sideways_shooter.aliens.sprites()[0]
 
+def test_alien_goes_up_and_down(mock_sideways_shooter, alien):
     alien.rect.center = mock_sideways_shooter.screen.get_rect().center
 
     init_pos = (alien.rect.x, alien.rect.y)
@@ -17,9 +20,8 @@ def test_alien_goes_up_and_down(mock_sideways_shooter):
     assert init_pos[0] == alien.rect.x
     assert init_pos[1] == alien.rect.y
 
-def test_alien_check_edges(mock_sideways_shooter):
-    alien = mock_sideways_shooter.aliens.sprites()[0]
 
+def test_alien_check_edges(mock_sideways_shooter, alien):
     mock_sideways_shooter.settings.fleet_speed = 100_000
     screen_height = mock_sideways_shooter.settings.screen_height
 
